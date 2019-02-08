147e19c005576dc3ac7fbd39f39d95666e6a7e26
Yahya <yahyadadda@gmail.com>
2019-02-08 22:20:26 +0100

    init the project

---
+++ src/main/java/ma/gi/TaxesProjectApplication.java
@@ -0,0 +1,14 @@
+package ma.gi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxesProjectApplication.class, args);
+	}
+
+}
+
